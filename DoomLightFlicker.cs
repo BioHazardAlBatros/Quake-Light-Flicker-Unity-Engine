257da72ac577f1fc33185e4760a14b96e2306243
BioHazardAlBatros <109250905+BioHazardAlBatros@users.noreply.github.com>
2024-02-21 20:35:04 +0300

    got rid of switch function, considering to add color change in the future

--- DoomLightFlicker.cs
+++ DoomLightFlicker.cs
@@ -1,5 +1,4 @@
-//Pros: Easy to create new presets, John Romero's algorithm, works on any object with Light Source.
-//Cons: Calculates animation only once during runtime, heavily depends on the FPS, really easy to break during coding, uses switches. I don't like them.
+//Pros: Easy to create new presets, works on any object with Light Source(won't work on BAKED lighting for obvious reasons).
 
 using System.Collections;
 using System.Collections.Generic;
@@ -7,226 +6,137 @@ using UnityEngine;
 
 public class DoomLightFlicker : MonoBehaviour
 {
-    //These variables are being used EVERY frame. Made some variables public so i can use them to create SCRIPTED sequences.
-    public int delay = 10;
     [SerializeField] float StartIntensity;
-    public float AddIntensity = 0;
-    //These variables are being used BEFORE first frame. Only once.
-    public string CustomAnim;
-    public int AnimationPreset;
-    //Not public variables.
-    int i;
-    float[] anim;
-    int fps=0;
+    [SerializeField] int delay;
+    [SerializeField] float AddIntensity;
+    [SerializeField] string CurrentAnim,NewCustomAnim;
+    [SerializeField] int AnimationPreset;
+    [SerializeField] bool Reinitialize;
+    int i,tick;
+    private float[] anim;
     Light lighting;
-    //Bool to update light animation.
-    public bool Reinitialize = false;
-    //Some preparations. Also starts animation generation.
+    
+    //I hope those will help someone with scripted events
+    void SetNewPreset(int NewValue)
+    {
+        AnimationPreset = NewValue;
+    }
+    void SetNewDelay(int NewValue)
+    {
+        delay = NewValue;
+    }
+    void SetNewCustomAnim(string Animation)
+    {
+        NewCustomAnim = Animation;
+    }
+    void SetNewStartIntensity(float NewValue) 
+    {
+        StartIntensity= NewValue;
+    }
+
     void Start()
     {
         lighting = GetComponent<Light>();
         StartIntensity = lighting.intensity;
-        GenerateAnimation(); 
+        GenerateAnimation();
     }
-    // This function animates Light. Why did i choose FixedUpdate? To make animation identical on every device/pc.
-void FixedUpdate()
+    // This function changes light intensity when number of ticks is equal to delay.
+    // Why did i choose FixedUpdate? To make animation look identical on every device/pc.
+    // UPD(02.20.24): Maybe i should consider using time.DeltaTime instead...
+    void FixedUpdate()
     {
-        //Used to update light animation.
+        //Generates new array with intensity values for animation if asked to
         if (Reinitialize)
-         GenerateAnimation();
-        //Restarts frame counter.
-        if (fps > delay)
-            fps = 0;
-        //Updates light intensity after delay. I recommend using 2-3 for SMOOTH animation. 5-10 for ABRUPT animation.
-        if (fps%delay==0)
+            GenerateAnimation();
+        //Updates light intensity after itdelay. 0-2  - Smooth Transition;
+        if (tick++ == delay)
         {
-            lighting.intensity = ((anim[i]*(StartIntensity+ AddIntensity)));
-            i++;
+            lighting.intensity = ((anim[i++] * StartIntensity) + AddIntensity);
             if (i == anim.Length)
             i = 0;
         }
-        fps++;
+        //Restarts frame counter.
+        if (tick > delay)
+            tick = 0;
     }
-    //This function reads CustomAnim value, if empty checks preset value and fills CustomAnim with symbols. Then starts Alphabet() function.
+    //This function reads CustomAnim value, if it's empty function checks preset value and fills CustomAnim with symbols,then translates them into floats.
     void GenerateAnimation()
     {
-        i = 0;
-        fps = 0;
-        if (CustomAnim == "")
+        if (NewCustomAnim == "")
         {
             switch (AnimationPreset)
             {
                 //Broken Light
                 case -1:
-                    CustomAnim = "a";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "a";
                     break;
                 //Normal Light
                 case 0:
-                    CustomAnim = "m";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "m";
                     break;
                 //Flicker 1
                 case 1:
-                    CustomAnim = "mmnmmommommnonmmonqnmmo";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "mmnmmommommnonmmonqnmmo";
                     break;
                 //Slow strong pulse
                 case 2:
-                    CustomAnim = "abcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcba";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "abcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcba";
                     break;
                 //Candle 1
                 case 3:
-                    CustomAnim = "mmmmmaaaaammmmmaaaaaabcdefgabcdefg";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "mmmmmaaaaammmmmaaaaaabcdefgabcdefg";
                     break;
                 //Fast Strobe
                 case 4:
-                    CustomAnim = "mamamamamama";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "mamamamamama";
                     break;
                 //Gentle Pulse
                 case 5:
-                    CustomAnim = "jklmnopqrstuvwxyzyxwvutsrqponmlkj";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "jklmnopqrstuvwxyzyxwvutsrqponmlkj";
                     break;
                 //Flicker 2
                 case 6:
-                    CustomAnim = "nmonqnmomnmomomno";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "nmonqnmomnmomomno";
                     break;
                 //Candle 2
                 case 7:
-                    CustomAnim = "mmmaaaabcdefgmmmmaaaammmaamm";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "mmmaaaabcdefgmmmmaaaammmaamm";
                     break;
                 //Candle 3
                 case 8:
-                    CustomAnim = "mmmaaammmaaammmabcdefaaaammmmabcdefmmmaaaa";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "mmmaaammmaaammmabcdefaaaammmmabcdefmmmaaaa";
                     break;
                 //Slow strobe 4
                 case 9:
-                    CustomAnim = "aaaaaaaazzzzzzzz";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "aaaaaaaazzzzzzzz";
                     break;
                 //Fluorescent Flicker
                 case 10:
-                    CustomAnim = "mmamammmmammamamaaamammma";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "mmamammmmammamamaaamammma";
                     break;
                 //Slow pulse not fade to black
                 case 11:
-                    CustomAnim = "abcdefghijklmnopqrrqponmlkjihgfedcba";
-                    anim = new float[CustomAnim.Length];
+                    NewCustomAnim = "abcdefghijklmnopqrrqponmlkjihgfedcba";
                     break;
-                //You can add your presets here. Here's the template.
-                //case 12:
-                //  CustomAnim = "WriteSomethingHere"; 
-                //  anim = new float[CustomAnim.Length];
-                //  break;
+                    //You can add your presets here.
             }
         }
         else
         {
-            anim = new float[CustomAnim.Length];
+            NewCustomAnim = NewCustomAnim.ToLower();
         }
-        foreach (int i in anim)
+        anim = new float[NewCustomAnim.Length];
+        CurrentAnim = NewCustomAnim;
+        for(int i=0;i<NewCustomAnim.Length;i++)
         {
-            Alphabet();
+            if ((96 < NewCustomAnim[i]) && (NewCustomAnim[i] < 123))
+                anim[i] = 0.083f * (NewCustomAnim[i] - 97);
+            else
+                anim[i] = 0f;
         }
         i = 0;
+        tick = 0;
         Reinitialize = false;
-        CustomAnim = "";        //Dunno why did i even add this line...
-    }
-    // Translates symbols to floats and puts them into array.
-    void Alphabet()
-    {
-        //Not elegant, i know. But atleast i didn't had to create any files. :D
-        switch (CustomAnim[i])
-        {
-            case 'a':
-                anim[i] = 0f;
-                break;
-            case 'b':
-                anim[i] = 0.083f;
-                break;
-            case 'c':
-                anim[i] = 0.167f;
-                break;
-            case 'd':
-                anim[i] = 0.25f;
-                break;
-            case 'e':
-                anim[i] = 0.33f;
-                break;
-            case 'f':
-                anim[i] = 0.417f;
-                break;
-            case 'g':
-                anim[i] = 0.5f;
-                break;
-            case 'h':
-                anim[i] = 0.583f;
-                break;
-            case 'i':
-                anim[i] = 0.667f;
-                break;
-            case 'j':
-                anim[i] = 0.75f;
-                break;
-            case 'k':
-                anim[i] = 0.833f;
-                break;
-            case 'l':
-                anim[i] = 0.917f;
-                break;
-            case 'm':
-                anim[i] = 1f;
-                break;
-            case 'n':
-                anim[i] = 1.083f;
-                break;
-            case 'o':
-                anim[i] = 1.167f;
-                break;
-            case 'p':
-                anim[i] = 1.25f;
-                break;
-            case 'q':
-                anim[i] = 1.33f;
-                break;
-            case 'r':
-                anim[i] = 1.417f;
-                break;
-            case 's':
-                anim[i] = 1.5f;
-                break;
-            case 't':
-                anim[i] = 1.583f;
-                break;
-            case 'u':
-                anim[i] = 1.667f;
-                break;
-            case 'v':
-                anim[i] = 1.75f;
-                break;
-            case 'w':
-                anim[i] = 1.833f;
-                break;
-            case 'x':
-                anim[i] = 1.917f;
-                break;
-            case 'y':
-                anim[i] = 2f;
-                break;
-            case 'z':
-                anim[i] = 2.083f;
-                break;
-            default: break;
-        }
-        //Counter.
-        i++;
+        NewCustomAnim = "";
     }
 }
